78e6d9e29b51ac3463621fbab44d296b18e375ce
angel <xxangel.singh@gmail.com>
2020-11-10 19:53:10 -0800

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Angel Singh on 2020-11-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
